e39c7b3853058d271e7307174a30867239830eb6
0ldCold <susskij14@gmail.com>
2022-01-02 16:57:38 +0600

    Поправил функции запроса к API

--- src/api/missionManifests.ts
+++ src/api/missionManifests.ts
@@ -1,8 +1,9 @@
 import { AxiosResponse } from "axios";
 import { instance, API_URL } from "./instance";
 
-export const getManifest = async (roverName: string): Promise<AxiosResponse<MissionApiData>> => {
-  return instance.get<MissionApiData>(`${API_URL}/manifests/${roverName}`);
+export const getManifest = async (roverName: string): Promise<Manifest> => {
+  const ApiResp = await instance.get<MissionApiData>(`${API_URL}/manifests/${roverName}`);
+  return ApiResp.data.photo_manifest;
 };
 
 export interface MissionApiData {
--- src/api/photosEndPoint.ts
+++ src/api/photosEndPoint.ts
@@ -7,8 +7,8 @@ export const getPhotosEndPoint = async (
   earth_date?: string,
   camera?: string,
   page?: number
-): Promise<AxiosResponse<PhotosApiData>> => {
-  return instance.get<PhotosApiData>(`${API_URL}/rovers/${roverName}/photos`, {
+): Promise<Photos> => {
+  const ApiResp = await instance.get<PhotosApiData>(`${API_URL}/rovers/${roverName}/photos`, {
     params: {
       earth_date,
       sol,
@@ -16,32 +16,26 @@ export const getPhotosEndPoint = async (
       page
     }
   });
+  return ApiResp.data.photos;
 };
-// , {
-//   params: {
-//     earth_date,
-//     camera,
-//     page
-//   }
-// }
 
 export interface PhotosApiData {
-  photos: Photos[];
+  photos: Photos;
 }
 
 export interface Photos {
-  id: number;
-  sol: number;
-  camera: {
+  [index: number]: {
     id: number;
-    name: string;
-    rover_id: number;
-    full_name: string;
-  };
-  img_src: string;
-  earth_date: string;
-  rover: {
-    [index: number]: {
+    sol: number;
+    camera: {
+      id: number;
+      name: string;
+      rover_id: number;
+      full_name: string;
+    };
+    img_src: string;
+    earth_date: string;
+    rover: {
       id: number;
       name: string;
       landing_date: string;
