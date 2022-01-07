2d58fceec477746e03092b97d216a0ef7b3a28ac
0ldCold <susskij14@gmail.com>
2022-01-08 04:57:14 +0600

    removed unnecessary components

--- src/scene/Manifest/ManifestPage.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import ManifestInfo from "components/custom/ManifestInfo/ManifestInfo";
-
-const ManifestPage: FC = () => {
-  return <ManifestInfo />;
-};
-
-export default ManifestPage;
